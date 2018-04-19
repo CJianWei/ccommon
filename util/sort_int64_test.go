35f984d0c9e4e2fcd270bab3fe10a56e7a2b1dc3
Jianwei Chen <chengjianwei2@banggood.com>
2018-04-19 09:21:17 +0800

    formate code

--- util/sort_int64_test.go
+++ util/sort_int64_test.go
@@ -9,5 +9,10 @@ import (
 func TestNewInt64Slice(t *testing.T) {
 	d := NewInt64Slice([]int64{6, 5, 4, 3, 2, 1})
 	sort.Sort(d)
+	if d.Data[0] > d.Data[1]{
+		t.Error("sort err")
+		return
+	}
 	logs.Informational("data :%v", d.Data)
 }
+
